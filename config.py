99eb67f556f49fdb591a1beb12fd9d7527591dad
YEOJU-SEOK <yeoju.ac@gmail.com>
2021-10-31 02:58:39 +0900

    add : template, static, view, form 추가

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ BASE_DIR = os.path.dirname(__file__)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 #sqlalchemy의 이벤트를 처리하는 옵션(필요하진 않아서 False)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "dev"
--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request
+from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
 
-from pybo import db
-from pybo.models import Question, Answer
+from .. import db
+from ..forms import AnswerForm
+from ..models import Question, Answer
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 """
@@ -15,11 +16,15 @@ request 객체 : 플라스크에서 생성 과정 없이 사용할 수 있는 �
 
 @bp.route('/create/<int:question_id>', methods=('POST',))
 def create(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    # <textarea name="content" id="content" rows="15"></textarea>에서 name이 content인걸 표시
-    content = request.form['content']
-    # answer의 경우 Answer(question=question, content=content, create_date=datetime.now())로 처리해도 괜춚
-    answer = Answer(content=content, create_date=datetime.now())
-    question.answer_set.append(answer) # db.session.add(answer)로 대체 가능
-    db.session.commit()
-    return redirect(url_for('question.detail', question_id=question_id))
+    if form.validate_on_submit():
+        # <textarea name="content" id="content" rows="15"></textarea>에서 name이 content인걸 표시
+        content = request.form['content']
+        # answer의 경우 Answer(question=question, content=content, create_date=datetime.now())로 처리해도 괜춚
+        answer = Answer(content=content, create_date=datetime.now())
+        question.answer_set.append(answer) # db.session.add(answer)로 대체 가능
+        db.session.commit()
+        return redirect(url_for('question.detail', question_id=question_id))
+    return render_template('question/question_detail.html', question=question, form=form)
+
