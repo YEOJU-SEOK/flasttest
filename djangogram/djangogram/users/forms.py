20bd07fb55741b8236da455fd045df2376907df3
YEOJU-SEOK <yeoju.ac@gmail.com>
2022-04-01 00:38:15 +0900

    css&html

--- djangogram/djangogram/users/forms.py
+++ djangogram/djangogram/users/forms.py
@@ -1,8 +1,7 @@
-from allauth.account.forms import SignupForm
-from allauth.socialaccount.forms import SignupForm as SocialSignupForm
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django import forms as django_forms
 
 User = get_user_model()
 
@@ -26,17 +25,25 @@ class UserAdminCreationForm(admin_forms.UserCreationForm):
         }
 
 
-class UserSignupForm(SignupForm):
-    """
-    Form that will be rendered on a user sign up section/screen.
-    Default fields will be added automatically.
-    Check UserSocialSignupForm for accounts created from social.
-    """
+class SignUpForm(django_forms.ModelForm):
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
 
-
-class UserSocialSignupForm(SocialSignupForm):
-    """
-    Renders the form when user has signed up using social accounts.
-    Default fields will be added automatically.
-    See UserSignupForm otherwise.
-    """
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'username', 'password']
+        # 화면에 나타난 각 필드별 명칭 변경
+        # labels = {
+        #     'email': '이메일주소', 'name': '성명', 'username': '사용자이름', 'password': '비밀번호'
+        # }
+        # 비밀번호 입력시 보이지 않게함
+        widgets = {
+            'email': django_forms.TextInput(attrs={'placeholder': '이메일 주소'}),
+            'name':  django_forms.TextInput(attrs={'placeholder': '이름'}),
+            'username': django_forms.TextInput(attrs={'placeholder': '사용자 이름'}),
+            'password' : django_forms.PasswordInput(attrs={'placeholder': '비밀번호'}),
+        }
