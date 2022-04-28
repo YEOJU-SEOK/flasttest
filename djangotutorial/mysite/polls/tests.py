56528c61bca7d0c8ef60d70b798bb0bdaca7ac5b
YEOJU-SEOK <yeoju.ac@gmail.com>
2022-04-29 02:15:30 +0900

    djangotutorial-testcaseadd

--- djangotutorial/mysite/polls/tests.py
+++ djangotutorial/mysite/polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -32,4 +33,55 @@ class QuestionModelTests(TestCase):
         self.assertIs(recent_question.was_published_recently(), True)
 
 
+# IndexView test
+def create_question(question_text, days):
+    """ 질문 등록날짜가 공개된건 음수, 공개되지 않은건 양수임"""
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_question(self):
+        """ 질문이 존재하지 않을경우, 적절한 메세지가 반환"""
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """ 과거날짜의 질문들이 인덱스페이지에 표시됨 """
+        question = create_question(question_text="Past question", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+
+    def test_future_question(self):
+        """ 미래날짜로 등록한 질문들이 인덱스페이지에 표시되는지 """
+        question = create_question(question_text="future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_two_past_question(self):
+        """하나의 인덱스페이지에 나오는 결과값이 복수인지 확인"""
+        question1 = create_question(question_text="past question1", days=-30)
+        question2 = create_question(question_text="past question2", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question2, question1])
+
+
+# DetailView Test
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """ 미래 날짜 설문 호출시 404에러"""
+        future_question = create_question(question_text='future question', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """ 과거 날짜 설문 호출시 해당 설문 보여줌 """
+        past_question = create_question(question_text='past question', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
--- djangotutorial/mysite/polls/views.py
+++ djangotutorial/mysite/polls/views.py
@@ -2,49 +2,55 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list' : latest_question_list,
-    }
-    # 동일한 기능 단축키로 render() 제공
-    # return HttpResponse(template.render(context, request))
-    # 인수 ; request, 탬플릿 이름, context(사전형)객체(optional)
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list' : latest_question_list,
+#     }
+#     # 동일한 기능 단축키로 render() 제공
+#     # return HttpResponse(template.render(context, request))
+#     # 인수 ; request, 탬플릿 이름, context(사전형)객체(optional)
+#     return render(request, 'polls/index.html', context)
 
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
+    # context_object_name : 기본적으로 장고는 object_list라는 변수에 해당 객체를 담는다, 뷰에서 템플릿 파일에 전달하는 컨텍스트 변수명을 지정한다.
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         # 가장 최근의 5개의 질문 리턴
-        return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5] # test
 
-
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-
-    # 단축키 get_object_or_404()
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#
+#     # 단축키 get_object_or_404()
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """ 아직 발행되지 않은 값은 반환되지 않게
+        (미래시점으로 등록한 설문은 실제로 보이지 않으나 url 노출 가능성이 있으므로 이를 수정해줌)"""
+        return Question.objects.filter(pub_date__lte=timezone.now()) # test
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {"question": question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {"question": question})
 
 
 class ResultsView(generic.DetailView):
